5e4cd4db2b58e8262a1fbc296f44b85fa2bddee9
anahych <acayo955@gmail.com>
2026-09-03 18:13:45 -0400

    Facture/ resta

--- aritmetica.py
+++ aritmetica.py
@@ -1,6 +1,10 @@
 import math
+
 def suma(a, b):
     return a + b
+
+def resta(a, b):
+    return a - b
 
 def raiz_cuadrada(a):
     return math.sqrt(a)
@@ -9,4 +13,4 @@ def cuadrado(a):
     return a ** 2
 
 def division(a, b):
-    return a / b
+    return a / b 
